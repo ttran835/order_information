b9f3c1e58179d752267be1c084a1827280922c4c
mlin367 <mlin367@yahoo.com>
2021-03-01 21:36:49 -0800

    Add count controller and wip for getOrders and getCsvs

--- server/controller/bigCommerceOrders.js
+++ server/controller/bigCommerceOrders.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const chalk = require('chalk');
 const Axios = require('axios');
+const { parseAsync } = require('json2csv');
 
 const bcUrlV2 = process.env.BC_API_PATH_V2;
 const bcUrlV3 = process.env.BC_API_PATH_V3;
@@ -26,7 +27,11 @@ const bigCommerceOrders = {
    */
   getOrders: async (req, res) => {
     try {
-      const { data } = await Axios.get(`${bcUrlV2}/orders`, optionsHeader);
+      const queryParams = (page) =>
+        ['sort=date_created:desc', `page=${page}`, 'limit=250'].join('&');
+
+      const { data } = await Axios.get(`${bcUrlV2}/orders?${queryParams(90)}`, optionsHeader);
+      console.log(data.length);
       if (data.length < 1) return res.sendStatus(400);
       res.status(200).send(data);
     } catch (err) {
@@ -40,12 +45,31 @@ const bigCommerceOrders = {
   getOrderProducts: async (req, res) => {
     try {
       const { orderId } = req.params;
-      const { data } = await Axios.get(`${bcUrlV2}/orders/${orderId}/products`, optionsHeader);
+      const { data } = await Axios.get(
+        `${bcUrlV2}/orders/${orderId}/products?sort=date_created:desc`,
+        optionsHeader,
+      );
       if (data.length < 1) return res.sendStatus(400);
       res.status(200).send(data);
     } catch (err) {
       console.log(chalk.redBg(JSON.stringify(err)));
     }
+  },
+
+  /**
+   * Receives order count
+   */
+  getOrderCount: async (req, res) => {
+    const { data } = await Axios.get(`${bcUrlV2}/orders/count`, optionsHeader);
+    if (data.length < 1) return res.sendStatus(400);
+    res.status(200).send(data);
+  },
+
+  /**
+   * Formats data and sends back csvs
+   */
+  getCsvs: async (req, res) => {
+
   },
 };
 
--- server/routes/bigCommerce/ordersRoutes.js
+++ server/routes/bigCommerce/ordersRoutes.js
@@ -3,5 +3,6 @@ const { bigCommerceOrders } = require('../../controller/bigCommerceOrders');
 
 OrdersRoutes.route('/get-all-orders').get(bigCommerceOrders.getOrders);
 OrdersRoutes.route('/:orderId/get-product-list').get(bigCommerceOrders.getOrderProducts);
+OrdersRoutes.route('/count').get(bigCommerceOrders.getOrderCount);
 
 module.exports = { OrdersRoutes };
