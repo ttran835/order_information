9efe07b9ba5fdadf9789868219282cb8b187f347
mlin367 <mlin367@yahoo.com>
2021-03-01 21:55:51 -0800

    Refactor getOrder and getOrderProducts request into resuable functions

--- server/controller/bigCommerceOrders.js
+++ server/controller/bigCommerceOrders.js
@@ -15,6 +15,32 @@ const optionsHeader = {
   },
 };
 
+const getAllOrdersFunc = async (page) => {
+  try {
+    const queryParams = ['sort=date_created:desc', `page=${page}`, 'limit=250'].join('&');
+
+    const { data } = await Axios.get(`${bcUrlV2}/orders?${queryParams}`, optionsHeader);
+
+    return data;
+  } catch (error) {
+    console.log('Error in getAllOrdersFunc');
+    throw error;
+  }
+};
+
+const getOrderProductsFunc = async (orderId) => {
+  try {
+    const { data } = await Axios.get(
+      `${bcUrlV2}/orders/${orderId}/products?sort=date_created:desc`,
+      optionsHeader,
+    );
+    return data;
+  } catch (error) {
+    console.log('Error in getOrderProductsFunc');
+    throw error;
+  }
+};
+
 /**
  * Requests to BigCommerce API can only be made via server
  * You cannot make requests on client side.
@@ -23,36 +49,33 @@ const optionsHeader = {
 
 const bigCommerceOrders = {
   /**
-   * Receive all orders from BigCommerce
+   * Receive all orders from BigCommerce and sends back headers csv
    */
   getOrders: async (req, res) => {
     try {
-      const queryParams = (page) =>
-        ['sort=date_created:desc', `page=${page}`, 'limit=250'].join('&');
-
-      const { data } = await Axios.get(`${bcUrlV2}/orders?${queryParams(90)}`, optionsHeader);
+      const { page } = req.query;
+      const data = await getAllOrdersFunc(page || 1);
       console.log(data.length);
       if (data.length < 1) return res.sendStatus(400);
       res.status(200).send(data);
-    } catch (err) {
-      console.log(chalk.redBg(JSON.stringify(err)));
+    } catch (error) {
+      res.sendStatus(400);
+      console.log(chalk.redBg(JSON.stringify(error)));
     }
   },
 
   /**
-   * Receive list of products from a specific order
+   * Receive list of products from a specific order and sends back details csv
    */
   getOrderProducts: async (req, res) => {
     try {
       const { orderId } = req.params;
-      const { data } = await Axios.get(
-        `${bcUrlV2}/orders/${orderId}/products?sort=date_created:desc`,
-        optionsHeader,
-      );
+      const data = await getOrderProductsFunc(orderId);
       if (data.length < 1) return res.sendStatus(400);
       res.status(200).send(data);
-    } catch (err) {
-      console.log(chalk.redBg(JSON.stringify(err)));
+    } catch (error) {
+      res.sendStatus(400);
+      console.log(chalk.redBg(JSON.stringify(error)));
     }
   },
 
@@ -60,17 +83,22 @@ const bigCommerceOrders = {
    * Receives order count
    */
   getOrderCount: async (req, res) => {
-    const { data } = await Axios.get(`${bcUrlV2}/orders/count`, optionsHeader);
-    if (data.length < 1) return res.sendStatus(400);
-    res.status(200).send(data);
+    try {
+      const { data } = await Axios.get(`${bcUrlV2}/orders/count`, optionsHeader);
+      if (data.length < 1) return res.sendStatus(400);
+      res.status(200).send(data);
+    } catch (error) {
+      res.sendStatus(400);
+    }
   },
 
-  /**
-   * Formats data and sends back csvs
-   */
   getCsvs: async (req, res) => {
+    try {
 
-  },
+    } catch(error) {
+
+    }
+  }
 };
 
 module.exports = { bigCommerceOrders };
