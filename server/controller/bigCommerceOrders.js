041937a1d4eb26cb4085e514b523b91023ae3b77
mlin367 <mlin367@yahoo.com>
2021-03-02 21:18:29 -0800

    Add min and max date for getOrdersFunc and add a get oldest year controller

--- server/controller/bigCommerceOrders.js
+++ server/controller/bigCommerceOrders.js
@@ -19,9 +19,16 @@ const optionsHeader = {
   },
 };
 
-const getAllOrdersFunc = async (page) => {
+// Dates need to be RFC-2822 or ISO-8601
+const getAllOrdersFunc = async (page, minDate, maxDate) => {
   try {
-    const queryParams = ['sort=date_created:desc', `page=${page}`, 'limit=250'].join('&');
+    const queryParams = [
+      'sort=date_created:desc',
+      `page=${page}`,
+      'limit=250',
+      ...(minDate ? [`min_date_created=${minDate}`] : []),
+      ...(maxDate ? [`max_date_created=${maxDate}`] : []),
+    ].join('&');
 
     const { data } = await Axios.get(`${bcUrlV2}/orders?${queryParams}`, optionsHeader);
 
@@ -34,13 +41,20 @@ const getAllOrdersFunc = async (page) => {
 
 const getOrderProductsFunc = async (orderId) => {
   try {
-    const { data } = await Axios.get(
-      `${bcUrlV2}/orders/${orderId}/products`,
-      optionsHeader,
-    );
+    const { data } = await Axios.get(`${bcUrlV2}/orders/${orderId}/products`, optionsHeader);
     return data;
   } catch (error) {
     console.log('Error in getOrderProductsFunc');
+    throw error;
+  }
+};
+
+const getOrdersCountFunc = async () => {
+  try {
+    const { data } = await Axios.get(`${bcUrlV2}/orders/count`, optionsHeader);
+    return data.count;
+  } catch (error) {
+    console.log('Error in getOrdersCountFunc');
     throw error;
   }
 };
@@ -60,7 +74,6 @@ const bigCommerceOrders = {
       const { page } = req.query;
       const data = await getAllOrdersFunc(page || 1);
       console.log(data.length);
-      if (data.length < 1) return res.sendStatus(400);
       res.status(200).send(data);
     } catch (error) {
       res.sendStatus(400);
@@ -75,7 +88,6 @@ const bigCommerceOrders = {
     try {
       const { orderId } = req.params;
       const data = await getOrderProductsFunc(orderId);
-      if (data.length < 1) return res.sendStatus(400);
       res.status(200).send(data);
     } catch (error) {
       res.sendStatus(400);
@@ -88,25 +100,56 @@ const bigCommerceOrders = {
    */
   getOrderCount: async (req, res) => {
     try {
-      const { data } = await Axios.get(`${bcUrlV2}/orders/count`, optionsHeader);
-      if (data.length < 1) return res.sendStatus(400);
+      const data = await getOrdersCountFunc();
       res.status(200).send(data);
     } catch (error) {
       res.sendStatus(400);
+      console.log(chalk.redBg(JSON.stringify(error)));
     }
   },
 
+  /**
+   * Sends back oldest year
+   */
+  getYearRange: async (req, res) => {
+    try {
+      // Get total order count to get the oldest record so we know what years
+      // are valid
+      const count = await getOrdersCountFunc();
+      const lastPage = Math.ceil(count / 250);
+      const lastOrder = (await getAllOrdersFunc(lastPage)).pop();
+      const oldestYear = new Date(lastOrder.date_created).getFullYear();
+
+      res.status(200).send(oldestYear);
+    } catch (error) {
+      res.sendStatus(400);
+      console.log(chalk.redBg(JSON.stringify(error)));
+    }
+  },
+
+  /**
+   * Sends back csv for either header or detail for specific time periods
+   */
   getCsvs: async (req, res) => {
     try {
       const { csvType, timePeriod, year } = req.params;
 
-      // First get all orders for frequency and year since both headers and details
+      const quarterly = [
+        TIME_PERIOD.JAN_TO_MARCH,
+        TIME_PERIOD.APRIL_TO_JUNE,
+        TIME_PERIOD.JULY_TO_SEPTEMBER,
+        TIME_PERIOD.OCTOBER_TO_DECEMBER,
+      ];
+
+      // First get all orders for timePeriod and year since both headers and details
       // rely on it
-
-    } catch(error) {
-
-    }
-  }
+      let minDate;
+      let maxDate;
+      if (quarterly.includes(timePeriod)) {
+        
+      }
+    } catch (error) {}
+  },
 };
 
 module.exports = { bigCommerceOrders };
