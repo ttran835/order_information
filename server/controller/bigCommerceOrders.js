df5b2d151d07959de4f15884b503f2193a17187f
mlin367 <mlin367@yahoo.com>
2021-03-02 21:51:37 -0800

    Write code to get min and max dates for each time period scenario

--- server/controller/bigCommerceOrders.js
+++ server/controller/bigCommerceOrders.js
@@ -59,6 +59,8 @@ const getOrdersCountFunc = async () => {
   }
 };
 
+const getLastDayOfMonth = (y, m) => new Date(y, m + 1, 0).getDate();
+
 /**
  * Requests to BigCommerce API can only be made via server
  * You cannot make requests on client side.
@@ -134,20 +136,52 @@ const bigCommerceOrders = {
     try {
       const { csvType, timePeriod, year } = req.params;
 
-      const quarterly = [
-        TIME_PERIOD.JAN_TO_MARCH,
-        TIME_PERIOD.APRIL_TO_JUNE,
-        TIME_PERIOD.JULY_TO_SEPTEMBER,
-        TIME_PERIOD.OCTOBER_TO_DECEMBER,
-      ];
+      const quarterlyMapping = {
+        [TIME_PERIOD.JAN_TO_MARCH]: { start: 0, end: 2 },
+        [TIME_PERIOD.APRIL_TO_JUNE]: { start: 3, end: 5 },
+        [TIME_PERIOD.JULY_TO_SEPTEMBER]: { start: 6, end: 8 },
+        [TIME_PERIOD.OCTOBER_TO_DECEMBER]: { start: 9, end: 11 },
+      };
 
-      // First get all orders for timePeriod and year since both headers and details
-      // rely on it
+      const monthMapping = {
+        [TIME_PERIOD.JANUARY]: 0,
+        [TIME_PERIOD.FEBRUARY]: 1,
+        [TIME_PERIOD.MARCH]: 2,
+        [TIME_PERIOD.APRIL]: 3,
+        [TIME_PERIOD.MAY]: 4,
+        [TIME_PERIOD.JUNE]: 5,
+        [TIME_PERIOD.JULY]: 6,
+        [TIME_PERIOD.AUGUST]: 7,
+        [TIME_PERIOD.SEPTEMBER]: 8,
+        [TIME_PERIOD.OCTOBER]: 9,
+        [TIME_PERIOD.NOVEMBER]: 10,
+        [TIME_PERIOD.DECEMBER]: 11,
+      };
+
+      // First calculate min and max dates to put for query params
       let minDate;
       let maxDate;
-      if (quarterly.includes(timePeriod)) {
-        
+      // Quarterly
+      if (Object.keys(quarterlyMapping).includes(timePeriod)) {
+        const { start, end } = quarterlyMapping[timePeriod];
+        minDate = new Date(year, start).toISOString();
+        maxDate = new Date(year, end, getLastDayOfMonth(year, end), 23, 59, 59).toISOString();
+
+        // Annually
+      } else if (timePeriod === TIME_PERIOD.ANNUAL) {
+        minDate = new Date(year, 0).toISOString();
+        maxDate = new Date(year, 11, getLastDayOfMonth(year, 11), 23, 59, 59).toISOString();
+
+        // Monthly
+      } else {
+        const month = monthMapping[timePeriod];
+        minDate = new Date(year, month).toISOString();
+        maxDate = new Date(year, month, getLastDayOfMonth(year, month), 23, 59, 59);
       }
+
+      // Get all orders for timePeriod and year since both headers and details
+      // rely on it
+      
     } catch (error) {}
   },
 };
