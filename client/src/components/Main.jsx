df587094a46cf10ab3782efe8cff29016b03dd46
mlin367 <mlin367@yahoo.com>
2021-03-03 22:22:51 -0800

    Drop downs grabbing correct values

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import axios from 'axios';
 
-import urls from './urls';
+import urls from '../_requests/urls';
 
 import { CSV_TYPE, TIME_PERIOD } from '../../../shared/fetchConstants';
+import { time } from 'faker';
 
 const FREQUENCY_TYPE = {
   MONTHLY: 'Monthly',
   QUARTERLY: 'Quarterly',
   ANNUALLY: 'Annually',
 };
+
+const MONTHS = [
+  TIME_PERIOD.JANUARY,
+  TIME_PERIOD.FEBRUARY,
+  TIME_PERIOD.MARCH,
+  TIME_PERIOD.APRIL,
+  TIME_PERIOD.MAY,
+  TIME_PERIOD.JUNE,
+  TIME_PERIOD.JULY,
+  TIME_PERIOD.AUGUST,
+  TIME_PERIOD.SEPTEMBER,
+  TIME_PERIOD.OCTOBER,
+  TIME_PERIOD.NOVEMBER,
+  TIME_PERIOD.DECEMBER,
+];
+
+const QUARTERS = [
+  TIME_PERIOD.JAN_TO_MARCH,
+  TIME_PERIOD.APRIL_TO_JUNE,
+  TIME_PERIOD.JULY_TO_SEPTEMBER,
+  TIME_PERIOD.OCTOBER_TO_DECEMBER,
+];
 
 class Main extends React.Component {
   constructor(props) {
@@ -18,6 +41,7 @@ class Main extends React.Component {
       validYears: [],
       frequencyType: FREQUENCY_TYPE.MONTHLY,
       timePeriod: TIME_PERIOD.JANUARY,
+      chosenYear: null,
     };
   }
 
@@ -31,30 +55,83 @@ class Main extends React.Component {
     for (let i = currentYear; i >= oldestYear; i--) {
       validYears.push(i);
     }
-    this.setState({ validYears });
+    this.setState({ validYears, chosenYear: currentYear });
+  };
+
+  onClickButtons = (csvType) => {
+    const { chosenYear, timePeriod } = this.state;
+    const url = `${urls.bcUrls}/csv/${csvType}/time-period/${timePeriod}/year/${chosenYear}`;
   };
 
   render() {
-    const { frequencyType } = this.state;
+    const { frequencyType, validYears } = this.state;
     return (
       <div>
         <h1>Download Headers Or Details CSV</h1>
-        <select onChange={(e) => this.setState({ frequencyType: e.target.value })}>
-          {Object.values(FREQUENCY_TYPE).map((freqType) => (
-            <option key={freqType} value={freqType}>
-              {freqType}
-            </option>
-          ))}
-        </select>
-        {frequencyType === FREQUENCY_TYPE.MONTHLY && (
-          <select onChange={(e) => this.setState({ timePeriod: e.target.value })}>
-            {Object.values(TIME_PERIOD).map((month) => (
-              <option value={month} key={month}>
-                {month}
+        <div style={{ display: 'flex' }}>
+          <h4>Year:</h4>
+          <select onChange={(e) => this.setState({ chosenYear: e.target.value })}>
+            {validYears.map((year) => (
+              <option value={year} key={year}>
+                {year}
               </option>
             ))}
           </select>
+        </div>
+        <>
+          <span>Frequency: </span>
+          <select
+            onChange={(e) => {
+              const chosenFrequencyType = e.target.value;
+              let timePeriod;
+              if (chosenFrequencyType === FREQUENCY_TYPE.ANNUALLY) {
+                timePeriod = TIME_PERIOD.ANNUAL;
+              } else if (chosenFrequencyType === FREQUENCY_TYPE.MONTHLY) {
+                timePeriod = MONTHS[0];
+              } else {
+                timePeriod = QUARTERS[0];
+              }
+
+              this.setState({
+                frequencyType: chosenFrequencyType,
+                timePeriod,
+              });
+            }}>
+            {Object.values(FREQUENCY_TYPE).map((freqType) => (
+              <option key={freqType} value={freqType}>
+                {freqType}
+              </option>
+            ))}
+          </select>
+        </>
+        {frequencyType === FREQUENCY_TYPE.MONTHLY && (
+          <>
+            <span>Month: </span>
+            <select onChange={(e) => this.setState({ timePeriod: e.target.value })}>
+              {MONTHS.map((month) => (
+                <option value={month} key={month}>
+                  {month}
+                </option>
+              ))}
+            </select>
+          </>
         )}
+        {frequencyType === FREQUENCY_TYPE.QUARTERLY && (
+          <>
+            <span>Month: </span>
+            <select onChange={(e) => this.setState({ timePeriod: e.target.value })}>
+              {QUARTERS.map((quarter) => (
+                <option value={quarter} key={quarter}>
+                  {quarter}
+                </option>
+              ))}
+            </select>
+          </>
+        )}
+        <div>
+          <button>Headers CSV</button>
+          <button>Details CSV</button>
+        </div>
       </div>
     );
   }
